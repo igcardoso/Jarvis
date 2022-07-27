19f03a9524f183aa71d95cf4f3a29b31f498021d
igor <85964276+igcardoso@users.noreply.github.com>
2022-07-27 09:47:43 -0300

    correction of lack of voice and writing response. Now she goes by Aurora

--- accets/js/comandos/comandos.js
+++ accets/js/comandos/comandos.js
@@ -3,14 +3,15 @@
 
   var speakBtn = document.querySelector('#speakbt');
   var resultSpeaker = document.querySelector('#resultSpeak');
-  var naoOuvi = new Audio('../accets/media/testeDeVoz.m4a');
-  var testeDeVoz = new Audio('../accets/media/testeDeVoz.m4a');
-  var aSuaDisposicao = new Audio('../accets/media/aSuaDisposicao.m4a');
-  var estouAqui = new Audio('../accets/media/estouAqui.m4a');
-  var Ok = new Audio('../accets/media/Ok.m4a');
-  var simSenhor = new Audio('../accets/media/simSenhor.m4a');
-  var comoQuiserSenhor = new Audio('../accets/media/comoQuiserSenhor.m4a');
-  var bemVindoSenhor = new Audio('../accets/media/bemVindoSenhor.m4a');
+  var naoOuvi = new Audio('../accets/media/audio/voz/naoOuvi.mp3');
+  var umMinuto = new Audio('../accets/media/audio/voz/umMinuto.mp3');
+  var voceSabeCantar = new Audio('../accets/media/audio/voz/voceSabeCantar.mp3');
+  var simSenhor = new Audio('../accets/media/audio/voz/simSenhor.mp3');
+  var voceFazOque = new Audio('../accets/media/audio/voz/voceFazOque.mp3');
+  var oiSenhor = new Audio('../accets/media/audio/voz/oiSenhor.mp3');
+  var estouAquiSenhor = new Audio('../accets/media/audio/voz/estouAquiSenhor.mp3');
+  var bemVindoSenhor = new Audio('../accets/media/audio/voz/bemVindoSenhor.mp3');
+  
 
   if (window.SpeechRecognition || window.webkitSpeechRecognition) {
 
@@ -19,10 +20,6 @@
     var myRecognition = new SpeechRecognition();
 
     myRecognition.lang = 'pt-BR';
-
-    $(document).ready(function() {
-      myRecognition.start();
-    });
 
     speakBtn.addEventListener('click', function () {
 
@@ -52,15 +49,12 @@
         case 'escurecer':
           document.body.style.backgroundColor = '#047751';
           break;
-        
+
         case 'vai descançar':
           myRecognition.stop();
           break;
         case 'acorda criança papai chegou':
           bemVindoSenhor.play();
-          resultSpeaker.innerHTML = 'Bem vindo senhor';
-          break;
-        case 'Aurora':
           resultSpeaker.innerHTML = 'Bem vindo senhor';
           break;
         case 't**********':
@@ -71,22 +65,69 @@
       if (resultSpeak.match(/buscar por/)) {
 
         resultSpeaker.innerHTML = 'Sim senhor';
-        
+
         setTimeout(function () {
-          simSenhor.play();
+          umMinuto.play();
           var resultado = resultSpeak.split('buscar por');
           window.location.href = 'https://www.google.com.br/search?q=' + resultado[1];
 
         }, 2000);
       }
       
+      if (resultSpeak.match(/O que é/)) {
+
+        resultSpeaker.innerHTML = 'Sim senhor';
+
+        setTimeout(function () {
+          umMinuto.play();
+          var resultado = resultSpeak.split('O que é');
+          window.location.href = 'https://www.google.com.br/search?q=O+que+é' + resultado[1];
+
+        }, 2000);
+      }
+      
+      if (resultSpeak.match(/Olá/)) {
+
+        resultSpeaker.innerHTML = 'oi';
+
+        setTimeout(function () {
+          oiSenhor.play();
+        }, 2000);
+      }
+      
+      if (resultSpeak.match(/Aurora/)) {
+
+        resultSpeaker.innerHTML = '...';
+
+        setTimeout(function () {
+          estouAquiSenhor.play();
+        }, 2000);
+      }
+      
+      if (resultSpeak.match(/você faz o quê/)) {
+
+        resultSpeaker.innerHTML = '...';
+
+        setTimeout(function () {
+          voceFazOque.play();
+        }, 2000);
+      }
+      
+      if (resultSpeak.match(/Você sabe cantar/)) {
+
+        resultSpeaker.innerHTML = '...';
+
+        setTimeout(function () {
+          voceSabeCantar.play();
+        }, 2000);
+      }
+
     },
       false);
 
     myRecognition.addEventListener('error',
       function (evt) {
         naoOuvi.play();
-        naoOuvi.controls();
         resultSpeaker.innerHTML = 'Se você disse alguma coisa, não ouvi muito bem!';
       },
       false);
